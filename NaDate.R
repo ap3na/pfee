7fa54bb8d02617ec81a66a3a30a8a859e2e8e023
Andres Pena <andrespena@gmail.com>
2023-02-06 18:58:22 -0600

    Delete NaDate.R

--- NaDate.R
+++
@@ -1,6 +0,0 @@
-#----------------------------------------------------------
-# function to put NA value to date if q value is NA
-# 
-NaDate <- function(date, q) {
-  x = if_else(is.na(q), ymd(NA), ymd(date))  
-}
