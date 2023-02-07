4f8049efd76be31e523c982b9532c9c30952b5e9
Andres Pena <andrespena@gmail.com>
2023-02-06 18:56:30 -0600

    Delete ExtractFieldWellsProd.R

--- ExtractFieldWellsProd.R
+++
@@ -1,7 +0,0 @@
-#----------------------------------------------------------
-# function to extract field wells production data
-#
-ExtractFieldWellsProd <- function(data, ftba)
-{
-     data %>% dplyr::filter(field %in% ftba)
-}
