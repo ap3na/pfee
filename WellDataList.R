da2bcf735ede783cfd396b0691e493a77babc24a
Andres Pena <andrespena@gmail.com>
2023-02-06 19:00:27 -0600

    Delete WellDataList.R

--- WellDataList.R
+++
@@ -1,9 +0,0 @@
-#----------------------------------------------------------
-# WellDataList function
-#
-WellDataList <- function(data) {
-  data %>%
-    select(well_name, t, Q) %>%
-    group_by(well_name) %>%
-    group_nest()
-}
