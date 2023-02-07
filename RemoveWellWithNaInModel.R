705cbaa2f2b629abedf79c1422e64d0ac2ca9992
Andres Pena <andrespena@gmail.com>
2023-02-06 18:59:52 -0600

    Delete RemoveWellWithNaInModel.R

--- RemoveWellWithNaInModel.R
+++
@@ -1,6 +0,0 @@
-#----------------------------------------------------------
-# function to remove well with NA in model  
-# 
-RemoveWellWithNaInModel <- function(well_data_list) {
-  well_data_list <- well_data_list[!is.na(well_data_list$nls_fit), ]     
-}
