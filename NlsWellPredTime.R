d7374b9a8701723632dafe83fc1ebc3f85ba6474
Andres Pena <andrespena@gmail.com>
2023-02-06 18:59:01 -0600

    Delete NlsWellPredTime.R

--- NlsWellPredTime.R
+++
@@ -1,9 +0,0 @@
-#----------------------------------------------------------
-# create NLS predict times
-#
-NlsWellPredTime <- function(well_data_list, nls_plots) {
-  tidyr::crossing(
-    well_name = well_data_list$well_name,
-    t = seq(min(nls_plots$t),
-            max(nls_plots$t) + fpt))
-}
