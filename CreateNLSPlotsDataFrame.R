33456311d5bdcb4caf3c25c36e270c9fbfb9d383
Andres Pena <andrespena@gmail.com>
2023-02-06 18:57:26 -0600

    Delete CreateNLSPlotsDataFrame.R

--- CreateNLSPlotsDataFrame.R
+++
@@ -1,8 +0,0 @@
-#----------------------------------------------------------
-# create NLS plots data frame 
-# 
-CreateNLSPlotsDataFrame <- function(data) {
-  data %>% 
-    select(well_name, data) %>%
-    unnest(data)     
-}
