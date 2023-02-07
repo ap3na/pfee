ed1f915ee50470d57a8abfb288ba722b4883e3b2
Andres Pena <andrespena@gmail.com>
2023-02-06 18:58:53 -0600

    Delete NlsCalculationForSeveralWells.R

--- NlsCalculationForSeveralWells.R
+++
@@ -1,9 +0,0 @@
-#----------------------------------------------------------
-# function to run NlsCalculation for several wells
-# 
-NlsCalculationForSeveralWells <- function(data) {
-  data %>% 
-    mutate(nls_fit = map(data, ~possibly(~NlsCalculation(.x), 
-                                         otherwise = NA,
-                                         quiet = TRUE)(.x)))     
-}
